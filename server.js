023e9cc9510f06e35d6821b3a484ba5459977b59
kingofassguard <psp09@my.yorku.ca>
2022-08-21 06:43:01 -0400

    Setup front page and server
    Still need to implement the password functionality.

--- server.js
+++ server.js
@@ -1,8 +1,44 @@
-const express = require('express');
+require("dotenv").config();
+const multer = require("multer");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const File = require("./models/File");
+
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+const upload = multer({dest: "uploads"})
 
-app.listen(3000);
+mongoose.connect(process.env.DATABASE_URL);
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+    const fileData = {
+        path: req.file.path,
+        originalName: req.file.originalname,
+    }
+    if(req.body.password!=null && req.body.password!==""){
+        fileData.password = await bcrypt.hash(req.body.password, 10);
+    }
+
+    const file = await File.create(fileData);
+
+    res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}`});
+});
+
+app.get("/file/:id", async (req, res) => {
+    const file = await File.findById(req.params.id);
+
+    file.downloadCount++;
+    await file.save();
+    console.log(file.downloadCount);
+
+    res.download(file.path, file.originalName);
+});
+
+app.listen(process.env.PORT); 
